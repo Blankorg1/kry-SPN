88c36a7e4f072f0ad4cbd990d1fcb26dbc611d2a
sandro.regli <sandro.regli@students.fhnw.ch>
2026-04-04 14:37:55 +0200

    chore: add comments for exercise

--- src/main.java
+++ src/main.java
@@ -1,4 +1,25 @@
 public class main {
+
+    /*
+    r = 4
+    n = 4
+    m = 4
+
+    S-Box: x    | 0 1 2 3 4 5 6 7 8 9 A B C D E F
+           S(x) | E 4 D 1 2 F B 8 3 A 6 C 5 9 0 7
+
+    Bitpermutation: 
+            0 1 2 3  4 5 6 7  8 9 10 11 12 13 14 15
+            0 4 8 12 1 5 9 13 2 6 10 14 3  7  11 15
+
+    K(k, i) bestehe aus den 16 aufeinanderfolgenden Bits von k beginnend bei Position 4i
+    k = 0011 1010 1001 0100 1101 0110 0011 1111
+
+    k0 = 0011 1010 1001 0100
+    k1 = 1010 1001 0100 1101
+    k2 = 1001 0100 1101 0110
+    k3 = 0100 1101 0110 0011
+     */
     public static void main(String arcs[]) {
         System.out.println("Kry is fun");
     }
