d6032e900463bca5eb8fddba9a121d3947048513
Benjamin Leuzinger <beleuz@gmx.ch>
2026-04-07 21:14:56 +0200

    decryptctr but somehow wrong?

--- src/main.java
+++ src/main.java
@@ -34,8 +34,9 @@ public class Main {
   // m = 4 wordcount
   // s = 32 keylength
 
-  // private static final String KEY = "00111010100101001101011000111111";
-  private static final String KEY = "00010001001010001000110000000000";
+  private static final String KEY = "00111010100101001101011000111111";
+  // test key
+  // private static final String KEY = "00010001001010001000110000000000";
   // private static final int R = 4; // number of rounds
   private static final int R = 4; // number of rounds
   private static final int N = 4; // bits per S-box input/output
@@ -58,28 +59,66 @@ public class Main {
   };
 
   public static void main(String[] args) throws IOException {
-    String chiffreText = readChiffreFile("./test.txt");
-    System.out.println(encrypt(chiffreText));
+    String chiffreText = readChiffreFile("./chiffre.txt");
+    String decryptedBits = decryptCTR(chiffreText);
+    String unpaddedChiffre = removePadding(decryptedBits);
+    System.out.println(bitsToAscii(unpaddedChiffre));
   }
 
-  public static String encrypt(String chiffreText) {
+  public static int encryptBlock(int block) {
+    block = xor(block, calcRoundKey(0));
+
+    // rounds 1-3
+    for (int round = 1; round < R; ++round) {
+      block = applySBox(block);
+      block = applyBitPermutation(block);
+      block = xor(block, calcRoundKey(round));
+    }
+
+    // final round
+    block = applySBox(block);
+    block = xor(block, calcRoundKey(R));
+
+    return block;
+  }
+
+  public static String decryptCTR(String chiffreText) {
+    String bitsResult = "";
+
+    for (int blockCount = 0; blockCount < (chiffreText.length() / BLOCK_SIZE); ++blockCount) {
+      int chiffreBlock = getChunkChiffre(chiffreText, blockCount);
+      int counterBlock = blockCount;
+      int keyStreamBlock = encryptBlock(counterBlock);
+      int textBlock = xor(chiffreBlock, keyStreamBlock);
+      String bits16 = String.format("%16s", Integer.toBinaryString(textBlock)).replace(' ', '0');
+      bitsResult += bits16;
+    }
+
+    return bitsResult;
+  }
+
+  private static String bitsToAscii(String bits) {
+    if (bits.length() % 8 != 0) {
+      throw new IllegalArgumentException("Yo this aint ascii");
+    }
+
+    String sb = "";
+    for (int i = 0; i < bits.length(); i += 8) {
+      int value = Integer.parseInt(bits.substring(i, i + 8), 2);
+      sb += ((char) value);
+    }
+
+    return sb;
+  }
+
+  // encrypts a chiffretext spn
+  private static String encrypt(String chiffreText) {
     String encrypted = "";
 
     for (int blockCount = 0; blockCount < (chiffreText.length() / BLOCK_SIZE); ++blockCount) {
       // initial step round 0
       int chiffreBlock = getChunkChiffre(chiffreText, blockCount);
-      chiffreBlock = xorRoundKey(chiffreBlock, calcRoundKey(0));
-
-      // rounds 1-3
-      for (int round = 1; round < R; ++round) {
-        chiffreBlock = applySBox(chiffreBlock);
-        chiffreBlock = applyBitPermutation(chiffreBlock);
-        chiffreBlock = xorRoundKey(chiffreBlock, calcRoundKey(round));
-      }
-
-      // final round
-      chiffreBlock = applySBox(chiffreBlock);
-      chiffreBlock = xorRoundKey(chiffreBlock, calcRoundKey(R));
+      chiffreBlock = encryptBlock(chiffreBlock);
 
       String bits16 = String.format("%16s", Integer.toBinaryString(chiffreBlock)).replace(' ', '0');
       encrypted += bits16;
@@ -87,6 +126,17 @@ public class Main {
 
     return encrypted;
   }
+
+  // removes until 1 found
+  private static String removePadding(String cipherText) {
+    int i = cipherText.length() - 1;
+    while (cipherText.charAt(i) == '0') {
+      i--;
+    }
+
+    return cipherText.substring(0, i);
+  }
+
 
   private static int applyBitPermutation(int chiffreBlock) {
     int result = 0;
@@ -110,8 +160,8 @@ public class Main {
     return Integer.parseUnsignedInt(KEY.substring(start, start + 16), 2);
   }
 
-  public static int xorRoundKey(int chiffreBlock, int roundKey) {
-    return chiffreBlock ^ roundKey;
+  public static int xor(int block, int key) {
+    return block ^ key;
   }
 
   public static int applySBox(int chiffreBlock) {
